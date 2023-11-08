dbe9e3bccd4a9e62fefc59d55c2a9cd932cd67f6
PawnBreak <d.dabbicco@duckma.com>
2023-11-08 12:54:28 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/hippocamp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hippocamp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/styles/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color primaryBlue = Color.fromRGBO(71, 130, 178, 1);
+
+  static final Color blue = Color.fromRGBO(0, 84, 147, 1);
+
+  static final Color primaryLightBlue = Color.fromRGBO(245, 249, 255, 1);
+
+  static final Color primaryRed = Color.fromRGBO(149, 17, 1, 1);
+
+  static final Color primaryYellow = Color.fromRGBO(254, 173, 32, 1);
+
+  static final Color primaryGrey = Color.fromRGBO(95, 94, 95, 1);
+
+  static final Color white243 = Color.fromRGBO(243, 243, 243, 1);
+
+  static final Color grey121 = Color.fromRGBO(121, 121, 121, 1);
+
+  static final Color grey66 = Color.fromRGBO(66, 66, 66, 1);
+
+  static final Color activeItem = Colors.blue.withOpacity(.5);
+
+  static final Color primaryLightGreen = Color.fromRGBO(241, 245, 223, 1);
+
+  static final Color pinkWhite = Color.fromRGBO(239, 230, 222, 1);
+
+  static final Color pinkWhiteDeep = Color.fromRGBO(204, 196, 190, 1);
+}
